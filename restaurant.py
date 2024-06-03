24e3c52ed8c5895206de480afcb4e5d071e668ba
Ekananda723 <71210723@students.ukdw.ac.id>
2024-06-03 20:04:13 +0700

    mengubah data menjadi dictionary, menampilkan data

--- restaurant.py
+++ restaurant.py
@@ -22,45 +22,46 @@ kernel.respond("resto")
 #     html += "</table>"
 #     return html
 
-data = [["Nasi Ayam Kremes", 14000], 
-            ["Nasi Ayam Kuah Kecap", 14000], 
-            ["Nasi Telur Kuah Kecap", 9000],
-            ["Nasi Telur Dadar", 7000],
-            ["Nasi Koloke", 12000],
-            ["Nasi Fuyunghai", 12000],
-            ["Nasi Telur Tempe Terong", 10000],
-            ["Nasi Tahu Terong", 18000],
-            ["Nasi Tahu Tempe Terong", 19000],
-            ["Nasi Tempe Terong", 8000],
-            ["Nasi Telur Terong", 9000],
-            ["Nasi Orak-Arik", 8000],
-            ["Nasi Gila", 13000],
-            ["Nasi Goreng Gila", 14000],
-            ["Nasi Goreng", 10000],
-            ["Bakmi Goreng/Kuah", 10000],
-            ["Bihun Goreng/Kuah", 10000],
-            ["Cap Cai", 10000],
-            ["Nasi Godog", 10000],
-            ["Mie Dok-dok", 10000],
-            ["Indomie", 6000],
-            ["Indomie Telur", 9000],
-            ["Es Teh", 3000],
-            ["Jeruk Es/Panas", 3000],
-            ["Milo Es/Panas", 4000],
-            ["Hilo Es/Panas", 4000],
-            ["Coffeemix Es/Panas", 4000],
-            ["Hilo Es/Panas", 4000],
-            ["GoodDay Capucino", 3000],
-            ["Nutrisari Es/Panas", 4000],
-            ["GoodDay", 4000],
-            ["Air Es", 1000],
-            ["Teh Tawar", 1000],
-            ["Kopi Hitam", 3000],
-            ]
+data = {
+    "Nasi Ayam Kremes": 14000,
+    "Nasi Ayam Kuah Kecap": 14000,
+    "Nasi Telur Kuah Kecap": 9000,
+    "Nasi Telur Dadar": 7000,
+    "Nasi Koloke": 12000,
+    "Nasi Fuyunghai": 12000,
+    "Nasi Telur Tempe Terong": 10000,
+    "Nasi Tahu Terong": 18000,
+    "Nasi Tahu Tempe Terong": 19000,
+    "Nasi Tempe Terong": 8000,
+    "Nasi Telur Terong": 9000,
+    "Nasi Orak-Arik": 8000,
+    "Nasi Gila": 13000,
+    "Nasi Goreng Gila": 14000,
+    "Nasi Goreng": 10000,
+    "Bakmi Goreng/Kuah": 10000,
+    "Bihun Goreng/Kuah": 10000,
+    "Cap Cai": 10000,
+    "Nasi Godog": 10000,
+    "Mie Dok-dok": 10000,
+    "Indomie": 6000,
+    "Indomie Telur": 9000,
+    "Es Teh": 3000,
+    "Jeruk Es/Panas": 3000,
+    "Milo Es/Panas": 4000,
+    "Hilo Es/Panas": 4000,
+    "Coffeemix Es/Panas": 4000,
+    "GoodDay Capucino": 3000,
+    "Nutrisari Es/Panas": 4000,
+    "GoodDay": 4000,
+    "Air Es": 1000,
+    "Teh Tawar": 1000,
+    "Kopi Hitam": 3000
+}
     #define header names
-col_names = ["Menu", "Harga"]
+
     #display table
-print(tabulate(data, headers=col_names))
+table = [[key, value] for key, value in data.items()]
+print(tabulate(table, headers=["Menu", "Harga"], tablefmt="grid"))
 
 while True:
     user_input = kernel.respond(input("USER > "))
